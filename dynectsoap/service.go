375af7b630ac1b97a990c4e46a98739aa8127184
Sanyu Melwani <1154057+sanyu@users.noreply.github.com>
2018-12-03 16:36:08 +1100

    Added generic Do call

--- dynectsoap/service.go
+++ dynectsoap/service.go
@@ -20,16 +20,15 @@ func NewDynect(client *Client) *dynectsoap {
 	}
 }
 
-func (service *dynectsoap) GetJobRetry(request *GetJobRequestType) (*GetJobResponseType, error) {
+func (service *dynectsoap) GetJobRetry(request *GetJobRequestType, response *GetJobResponseType) error {
 	var err error
-	response := new(GetJobResponseType)
 
 	for {
 		select {
 		case <-time.After(PollingInterval):
 			err = service.client.Call(ApiUrl, request, response)
 			if err != nil {
-				return nil, err
+				return err
 			}
 
 			realResponse := response.Data.(JobResponseInterface)
@@ -41,72 +40,15 @@ func (service *dynectsoap) GetJobRetry(request *GetJobRequestType) (*GetJobRespo
 				log.Debugf("Retrieving Job %v again, status is incomplete", response.Job_id)
 				continue
 			default:
-				return response, nil
+				return nil
 			}
 		}
 	}
 
-	return response, nil
+	return nil
 }
 
-func (service *dynectsoap) GetAllRecords(request *GetAllRecordsRequestType) (*GetAllRecordsResponseType, error) {
-	response := new(GetAllRecordsResponseType)
-
+func (service *dynectsoap) Do(request, response interface{}) error {
 	err := service.client.Call(ApiUrl, request, response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (service *dynectsoap) GetJob(request *GetJobRequestType) (*GetJobResponseType, error) {
-	response := new(GetJobResponseType)
-
-	err := service.client.Call(ApiUrl, request, response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (service *dynectsoap) SessionLogin(request *SessionLoginRequestType) (*SessionLoginResponseType, error) {
-	response := new(SessionLoginResponseType)
-	err := service.client.Call(ApiUrl, request, response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (service *dynectsoap) SessionLogout(request *SessionLogoutRequestType) (*SessionLogoutResponseType, error) {
-	response := new(SessionLogoutResponseType)
-	err := service.client.Call(ApiUrl, request, response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (service *dynectsoap) SessionIsAlive(request *SessionIsAliveRequestType) (*SessionIsAliveResponseType, error) {
-	response := new(SessionIsAliveResponseType)
-	err := service.client.Call(ApiUrl, request, response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (service *dynectsoap) SessionKeepAlive(request *SessionKeepAliveRequestType) (*SessionKeepAliveResponseType, error) {
-	response := new(SessionKeepAliveResponseType)
-	err := service.client.Call(ApiUrl, request, response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
+	return err
 }
